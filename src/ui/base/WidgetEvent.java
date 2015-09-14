9860b589220837d9b551244f6136e64f98959a6a
hyfu <71806228@qq.com>
2015-09-14 08:30:01 +0800

    m

--- src/ui/base/WidgetEvent.java
+++ src/ui/base/WidgetEvent.java
@@ -2,6 +2,36 @@ package ui.base;
 
 public interface WidgetEvent {
 	
+	public enum EventType {
+		
+		EVENT_MOUSE_ENTERED,
+
+		EVENT_MOUSE_EXITED,
+
+		EVENT_MOUSE_PRESSED,
+
+		EVENT_MOUSE_RELEASED,
+
+		EVENT_MOUSE_CLICKED,
+
+		EVENT_MOUSE_MOVED,
+
+		EVENT_MOUSE_DRAGGED,
+		
+		EVENT_KEY_TYPED,
+
+		EVENT_KEY_PRESSED,
+
+		EVENT_KEY_RELEASED,
+
+		EVENT_MOUSE_RIGHT,
+
+		EVENT_MOUSE_MIDDLE,
+
+		EVENT_MOUSE_LEFT;
+		
+	}
+	
 	int getX();
 	
 	int getY();
